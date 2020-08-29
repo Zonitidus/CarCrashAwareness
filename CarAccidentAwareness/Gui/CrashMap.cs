619717d64518ccc6721f7bd1c52d78aee44153f9
Zonitidus <zonitidus@gmail.com>
2020-08-28 19:32:59 -0500

    Basic project structure.

---
+++ CarAccidentAwareness/Gui/CrashMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarAccidentAwareness
+{
+    public partial class CrashMap : Form
+    {
+        public CrashMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
