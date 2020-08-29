eb17509bc26f4976e3cc7565ecffc9e9259bafc6
Juan Ossa <juan.ossa@hotmail.com>
2020-08-28 23:42:19 -0500

    Correcting some Loading Data Bugs

--- CarAccidentAwareness/Model/DataManager.cs
+++ CarAccidentAwareness/Model/DataManager.cs
@@ -10,13 +10,20 @@ namespace CarAccidentAwareness.Model
         DataTable dt = new DataTable();
         public DataTable GetDataTable(String path)
         {
-            List<List<String>> data = ReadData(path);   
+            List<List<String>> data = ReadData(path);
             for (int i = 0; i < data.Count; i++)
             {
                 DataRow row = dt.NewRow();
-                for (int j = 0; j < data.Count; j++)
+
+
+                for (int j = 0; j < data.Count - 1; j++)
                 {
                     row[j] = data[i][j];
+                    //row[1] = data[i][1];
+                    //row[2] = data[i][2];
+                    //row[3] = data[i][3];
+                    //row[4] = data[i][4];
+                    //row[j] = data[i][0];
                 }
                 dt.Rows.Add(row);
             }
@@ -26,32 +33,26 @@ namespace CarAccidentAwareness.Model
         {
             StreamReader reader = new StreamReader(path);
             List<List<String>> temp = new List<List<string>>();
+            String line = reader.ReadLine();
+            String[] dataItem = line.Split(',');
+            for (int g = 0; g < dataItem.Length; g++)
+            {
+                CreateDataTable(dataItem[g]);
+            }
             while (!reader.EndOfStream)
             {
-                int f = 0;
-                String line = reader.ReadLine();
-                String[] dataItem = line.Split(',');
-                if (f==0) 
+                line = reader.ReadLine();
+                dataItem = line.Split(',');
+                List<String> item = new List<string>();
+                for (int i = 0; i < dataItem.Length; i++)
                 {
-                    for (int g=0;g<dataItem.Length;g++) 
-                    {
-                        CreateDataTable(dataItem[0]);
-                    }
-                }    
-                if (f!=0)//else?
-                {
-                    List<String> item = new List<string>();
-                    for (int i = 0; i < dataItem.Length; i++)
-                    {
-                        item.Add(dataItem[i]);
-                    }
-                    temp.Add(item);
+                    item.Add(dataItem[i]);
                 }
-                f++;
+                temp.Add(item);
             }
             return temp;
         }
-        private void CreateDataTable(string columnName) 
+        private void CreateDataTable(string columnName)
         {
             DataColumn column = new DataColumn();
             column.DataType = System.Type.GetType("System.String");
